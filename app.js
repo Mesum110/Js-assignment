2d67bfab0efd3068a57a14779ef6ca7787a77f04
MeAsum raza <razameer1100@gmail.com>
2020-06-14 19:48:19 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,258 @@
+// ======Chapter 1
+// alert("Welcome to Js Code Land");
+// alert("Welcome to Js Code World");
+// alert("Welcome to Js World\nEnjoy Coding");
+// document.write("Welcome to Js Code Land" + "<br>");
+// console.log("Welcome to Js Code Land");
+
+
+// ======Chapter 2
+// var firstName = "Mesum";
+// var lastName = "Raza";
+// alert("My Name is" +" " + firstName + " "+ lastName);
+// var email = "Measumraza1100@gmail.com";
+// var age=24;
+// alert( email);
+// alert(age);
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+// ======Chapter 3
+
+// var age=24;
+// alert("My age is" +" " + age);
+// var birthyear= 1996;
+// document.write("My birth year is" +" "+ birthyear +"<br>");
+// document.write("Data type of birth year variable is" + " " + typeof birthyear);
+
+// ======Chapter 4
+// document.write("<h1> “Rules for naming JS variables”</h1>");
+// var one = "letter";
+// var two = "underscore (_)";
+// var three = "dollar sign ($).";
+// var four ="sensitive";
+// document.write("Variable names can only contain"+" "+ one +" "+ two+"and "+three +"<br>");
+// document.write("Variable names are case"+" "+ four +"<br>" );
+
+// var one1 = "numbers";
+// var two1 = "reserved words";
+// var three1 = "space";
+// document.write("These are illegal Variables"+" "+ one1 +" "+ two1+"and "+ " "+three1 +"<br>");
+
+// ======Chapter 5
+
+
+    // var a=15;
+    // var b=30;
+    // var c=a+b;
+    // var d=a-b;
+    // var e=a*b;
+    // var f=a/b;
+    // var g=b%a;
+    // document.write("My Sum of 15 and 30 is" +" "+c +"<br>"); 
+    // document.write("My Sub 15 and 30 is" +" "+d +"<br>"); 
+    // document.write("My Multiplication 15 and 30 is" +" "+e +"<br>"); 
+    // document.write("My Modulas 15 and 30 is" +" "+g +"<br>");
+    // var h=600;
+    // document.write("The total cost of moive is" +" "+ 5 * h + "<br>");
+//     for (var i = 1; i < 11; i++) {
+// document.write("4" + "x" +i +"=" + i*4 +"<br>")
+//         }
+
+//   var celsius = 25;
+//   var far= (celsius*9/5)+32;
+
+//   document.write("25' C is" +" "+far + "F");
+
+// ======Chapter 6-9
+
+// var a = 45;
+// document.write("The value is" +" " + a +"<br>");
+// var result = --a ;
+// document.write("The value --a result is" +" " + result);
+// var a = 2, b = 6;
+// var result = --a + --b + ++b + b--;
+// document.write(result);
+
+// var j= window.prompt("sometext");
+//     for (var i = 1; i < 11; i++) {
+// document.write(j + "x" +i +"=" + i*j +"<br>")
+        // }
+
+// ======Chapter 9-11
+
+
+
+//  var x = prompt("Enter City Name");
+//  if (x === "Karachi") {
+//       document.write("Welcome to the city of light");
+//  }
+//  if (x !== "Karachi") {
+//     document.write("Welcome to the city");
+//  }
+
+// var x = prompt("Enter Gender");
+// if (x === "Male") {
+//      document.write("Good morning sir");
+// }
+// if (x === "Female") {
+//    document.write("Good morning madam");
+// }
+// var x = prompt("Enter Trafic Light color");
+// if (x === "Green") {
+//      document.write("Go now");
+// }
+// if (x === "Red") {
+//    document.write("stop");
+// }
+// if(x === "Yellow") {
+//     document.write("Ready to GO");
+//  }
+
+// var x = prompt("Enter Fuel");
+//  if (x <= 0.25) {
+//     alert("Please Refill the fuel in your car");
+//      }
+//      else {
+//      alert("Fuel is above from below level");
+//      }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is  true");
+// }
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+    
+// var a = +prompt("Enter Subject 1");
+// var b=+ prompt("Enter Subject 2");
+// var c =+ prompt("Enter Subject 3");
+// var d= a+b+b;
+// var total=300;
+// var per= d/total*100;
+// document.write("Total Marks " +" " + total + "<br>");
+// document.write("Obtain Marks are " +" " + d + "<br>");
+// document.write("Percentage  is " +" " + per + "<br>");
+// if (per >= 80 && per <= 100){
+//     document.write("Excelent");
+// }
+//   else if (per >= 60 && per <= 79){
+//     document.write("Good");
+// }
+//   else if (per >= 40 && per <= 69){
+//     document.write("Fair");
+// }
+// else {
+//     document.write("Fail"); 
+//   }
+
+
+
+// ======Chapter 12-13
+
+// var a = +prompt("Enter Number 1");
+// var b=+ prompt("Enter Number 2");
+// if (a>b) {
+//     document.write(a)
+    
+// } else if (a==b){
+//         document.write("both number are same");
+//     }
+//     else {
+//         document.write(b)
+//     }
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// document.write("Good day");
+// }
+// else{
+//  document.write("Good evening");
+// }
+
+// var user =prompt("Time");
+// if (user =0000 && user <= 1200){
+//     document.write("G00d Morning");
+// }
+//   else if (user >= 1200 && user <= 1700){
+//     document.write("Good Afternoon");
+// }
+//   else if (user >= 1700 && user <= 2100){
+//     document.write("Good Evening");
+// }
+// else if (user >= 2100 && user <= 2359){
+//     document.write("Good Nite");
+// }
+
+
+
+// ======Chapter 14-16
+
+
+// var arr = [[],[]];
+
+// arr=["a","b" ,"c" , "d"];
+// document.write(arr);
+
+// arr=[1, 2,3 ,4];
+// document.write(arr);
+
+// arr=[0,1];
+// document.write(arr);
+
+// arr=[0,1,"abc","abcd"];
+// document.write(arr);
+
+// var degrees = [ "SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS"," M. Phil.", "PhD"]
+//     document.write(degrees);
+
+
+// var student=["ali","hamza","raza"];
+// var pert=["90%"]
+// for (var i=0; i< student.length; i++){
+//     for(var x=0; x<pert.length; x++){
+//         document.write(student[i]+" "+ pert[x] +"<br>");
+//     }
+
+
+// }
+
+// var cities=["Karachi","Lahore","queta","hyderbad","moro"];
+// var select=cities.slice(1,3);
+// document.write(select);
+
+var points = [40, 100, 1, 5, 25, 10];
+points.sort();
